0eeefe708397be40f03f68ce10069a6c91f389af
ckonoz <cjkonoza@gmail.com>
2015-12-23 13:28:08 -0500

    category edit and image delete

--- app/server/controllers/categories/controller.js
+++ app/server/controllers/categories/controller.js
@@ -1,4 +1,3 @@
-
 /*
  * Corinne Konoza
  *  7.21.15
@@ -6,156 +5,219 @@
  */
 
 var Category = require('../../models/categories/model.js'),
-    Product = require('../../models/products/model.js'),
-    fs = require('fs');
+  Product = require('../../models/products/model.js'),
+  fs = require('fs');
 
-exports.createForm = function (req, res) {
-    var breadcrumbs = [
-        {title: 'Dashboard', link: '/dashboard/'},
-        {title: 'Categories', link: '/categories/'},
-        {title: 'Create Category'}
-    ];
+exports.createForm = function(req, res) {
+  var breadcrumbs = [{
+    title: 'Dashboard',
+    link: '/dashboard/'
+  }, {
+    title: 'Categories',
+    link: '/categories/'
+  }, {
+    title: 'Create Category'
+  }];
 
-    res.render('categories/forms/create', {title: 'Create Category', breadcrumbs: breadcrumbs});
+  res.render('categories/forms/create', {
+    title: 'Create Category',
+    breadcrumbs: breadcrumbs
+  });
 };
 
-exports.get = function (req, res) {
-    var breadcrumbs = [
-        {title: 'Dashboard', link: '/dashboard/'},
-        {title: 'Categories'}
-    ];
+exports.get = function(req, res) {
+  var breadcrumbs = [{
+    title: 'Dashboard',
+    link: '/dashboard/'
+  }, {
+    title: 'Categories'
+  }];
 
-    Category.find({}, function(err, categories) {
-        if (err) { console.log(err);}
-        res.render('categories/list', {title: 'Categories', categories: categories, breadcrumbs: breadcrumbs});
+  Category.find({}, function(err, categories) {
+    if (err) {
+      console.log(err);
+    }
+    res.render('categories/list', {
+      title: 'Categories',
+      categories: categories,
+      breadcrumbs: breadcrumbs
     });
+  });
 
 };
 
-exports.delete = function (req, res) {
-    Category.findOne({_id: req.query._id}, function (err, category) {
-        if(err){console.log(err);}
-        Product.find({category: category.identity}, function (err, products) {
-            if(err){console.log(err);}
+exports.delete = function(req, res) {
+  Category.findOne({
+    _id: req.query._id
+  }, function(err, category) {
+    if (err) {
+      console.log(err);
+    }
+    Product.find({
+      category: category.identity
+    }, function(err, products) {
+      if (err) {
+        console.log(err);
+      }
 
-            products.forEach( function(element){
-                element.remove();
+      products.forEach(function(element) {
+        element.remove();
+      });
+
+      category.delete_image(category.logo, function(err) {
+        if (err) {
+          console.log(err);
+        }
+        category.remove();
+
+        res.redirect('/categories/');
+      });
+    });
+  });
+};
+
+
+exports.create = function(req, res) {
+  var breadcrumbs = [{
+    title: 'Dashboard',
+    link: '/dashboard/'
+  }, {
+    title: 'Categories',
+    link: '/categories/'
+  }, {
+    title: 'Create Category'
+  }];
+
+  Category.findOne({
+    identity: req.body.identity
+  }, function(err, found) {
+    if (found == null) {
+      var new_url = req.body.segment_url.split(' ').join('_');
+
+      var newCategory = new Category({
+        identity: req.body.identity,
+        segment_url: new_url,
+        key_words: req.body.key_words,
+        description: req.body.description,
+        cont: req.body.cont
+      });
+
+      newCategory.save(function(err, category) {
+        if (!req.files.logo) {
+          return res.redirect('/categories');
+        }
+        newCategory.save_image(req.files.logo, function(err) {
+          if (err) {
+            var error = "Oops! There was a problem creating your category. Please try again.";
+            res.render('categories/forms/create', {
+              title: 'Create Category',
+              breadcrumbs: breadcrumbs,
+              error: error
             });
+          } else {
+            res.redirect("/categories");
+          }
+        })
+      });
+    } else {
+      var error = "Oops! A category with that name is in our system. Please create another.";
+      res.render('categories/forms/create', {
+        title: 'Create Category',
+        breadcrumbs: breadcrumbs,
+        error: error
+      });
+    }
+  });
+};
 
-            category.remove();
-            res.redirect('/categories/');
+exports.updateForm = function(req, res) {
+  var breadcrumbs = [{
+    title: 'Dashboard',
+    link: '/dashboard/'
+  }, {
+    title: 'Categories',
+    link: '/categories/'
+  }, {
+    title: 'Edit Category'
+  }];
 
+  Category.findOne({
+    _id: req.query._id
+  }, function(err, category) {
+    if (err) {
+      console.log(err);
+    }
+    res.render('categories/forms/edit', {
+      title: 'Edit Category',
+      category: category,
+      breadcrumbs: breadcrumbs
+    });
+  });
+};
+
+
+exports.update = function(req, res) {
+
+  Category.findOne({
+    _id: req.body._id
+  }, function(err, category) {
+    var new_url = req.body.segment_url.split(' ').join('_');
+
+    if ((req.body.delete_logo == "on")) {
+
+      Product.find({
+        category: category.identity
+      }, function(err, products) {
+        products.forEach(function(product) {
+          product.category = req.body.identity;
+          product.save();
         });
-    });
+
+        category.identity = req.body.identity;
+        category.segment_url = new_url;
+        category.key_words = req.body.key_words;
+        category.description = req.body.description;
+        category.cont = req.body.cont;
+
+        fs.unlinkSync('./public' + category.logo);
+
+        category.save(function(err, category) {
+          if (!req.files.new_logo) {
+            return res.redirect('/categories');
+          }
+          category.save_image(req.files.new_logo, function(err) {
+            if (err) {
+              console.log(err);
+            }
+            res.redirect("/categories");
+          })
+        });
+      });
+
+
+    } else {
+
+      Product.find({
+        category: category.identity
+      }, function(err, products) {
+        products.forEach(function(product) {
+          product.category = req.body.identity;
+          product.save();
+        });
+
+        category.identity = req.body.identity;
+        category.segment_url = new_url;
+        category.key_words = req.body.key_words;
+        category.description = req.body.description;
+        category.cont = req.body.cont;
+
+        category.save(function(err, category) {
+          if (err) {
+            console.log(err);
+          }
+          return res.redirect('/categories');
+        });
+      });
+    }
+  });
 };
-
-
-exports.create = function (req, res) {
-    var breadcrumbs = [
-        {title: 'Dashboard', link: '/dashboard/'},
-        {title: 'Categories', link: '/categories/'},
-        {title: 'Create Category'}
-    ];
-
-    Category.findOne({identity: req.body.identity}, function (err, found) {
-        if (found == null) {
-            var new_url = req.body.segment_url.split(' ').join('_');
-
-            var newCategory = new Category({
-                identity: req.body.identity,
-                segment_url: new_url,
-                key_words: req.body.key_words,
-                description: req.body.description,
-                cont: req.body.cont
-            });
-
-            newCategory.save( function (err, category) {
-                if (!req.files.logo) {
-                    return res.redirect('/categories');
-                }
-                newCategory.save_image(req.files.logo, function (err) {
-                    if (err) {
-                        var error = "Oops! There was a problem creating your category. Please try again.";
-                        res.render('categories/forms/create', {title: 'Create Category', breadcrumbs: breadcrumbs, error: error });
-                    }else {
-                        res.redirect("/categories");
-                    }
-                })
-            });
-        } else {
-            var error = "Oops! A category with that name is in our system. Please create another.";
-            res.render('categories/forms/create', {title: 'Create Category', breadcrumbs: breadcrumbs, error: error });
-        }
-    });
-};
-
-exports.updateForm = function (req, res) {
-    var breadcrumbs = [
-        {title: 'Dashboard', link: '/dashboard/'},
-        {title: 'Categories', link: '/categories/'},
-        {title: 'Edit Category'}
-    ];
-
-    Category.findOne({_id: req.query._id}, function (err, category) {
-        if(err){console.log(err);}
-        res.render('categories/forms/edit', {title: 'Edit Category', category: category, breadcrumbs: breadcrumbs});
-    });
-};
-
-
-exports.update = function (req, res) {
-
-    Category.findOne({_id: req.body._id}, function (err, category) {
-        var new_url = req.body.segment_url.split(' ').join('_');
-
-        if ((req.body.delete_logo == "on")){
-
-            Product.find({category: category.identity}, function (err, products) {
-                products.forEach(function(product) {
-                    product.category = req.body.identity;
-                    product.save();
-                });
-
-                category.identity = req.body.identity;
-                category.segment_url = new_url;
-                category.key_words = req.body.key_words;
-                category.description = req.body.description;
-                category.cont = req.body.cont;
-
-                fs.unlinkSync('./public' + category.logo);
-
-                category.save( function (err, category) {
-                    if (!req.files.new_logo) {
-                        return res.redirect('/categories');
-                    }
-                    category.save_image(req.files.new_logo, function (err) {
-                        if(err){console.log(err);}
-                        res.redirect("/categories");
-                    })
-                });
-            });
-
-
-        } else {
-
-            Product.find({category: category.identity}, function (err, products) {
-                products.forEach(function (product) {
-                    product.category = req.body.identity;
-                    product.save();
-                });
-
-                category.identity = req.body.identity;
-                category.segment_url = new_url;
-                category.key_words = req.body.key_words;
-                category.description = req.body.description;
-                category.cont = req.body.cont;
-
-                category.save(function (err, category) {
-                    if(err){console.log(err);}
-                    return res.redirect('/categories');
-                });
-            });
-        }
-    });
-};
-
--- app/server/models/categories/model.js
+++ app/server/models/categories/model.js
@@ -6,32 +6,62 @@
 
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    fs = require('fs'),
-    path = require('path');
+  Schema = mongoose.Schema,
+  fs = require('fs'),
+  path = require('path');
 
 // Define CategorySchema with the various fields that we want to store
 // in MongoDB for each category in the store
 var CategorySchema = new Schema({
-    identity: { type: String, required: true },
-    segment_url: { type: String, required: true , unique: true },
-    key_words: { type: String, required: true },
-    description: { type: String, required: true },
-    logo: { type: String, required: true },
-    cont: { type: String, required: true }
+  identity: {
+    type: String,
+    required: true
+  },
+  segment_url: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  key_words: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  logo: {
+    type: String,
+    required: true
+  },
+  cont: {
+    type: String,
+    required: true
+  }
 });
 
 CategorySchema.methods.save_image = function(file, cb) {
 
-    var $this = this;
-    console.log("File:" + file);
-   fs.rename(file.path, './public/images/logos/' + file.name, function (err) { // system path
-        if (err) {
-            return cb(err);
-        }
-       $this.logo = 'public/images/logos/' + file.name; // browser path
-       $this.save(cb);
-    });
+  var $this = this;
+  console.log("File:" + file);
+  fs.rename(file.path, './public/images/logos/' + file.name, function(err) { // system path
+    if (err) {
+      return cb(err);
+    }
+    $this.logo = '/images/logos/' + file.name; // browser path
+    $this.save(cb);
+  });
+};
+
+CategorySchema.methods.delete_image = function(file, cb) {
+  console.log(file);
+  var $this = this;
+  fs.unlink('./public' + file, function(err) {
+    if (err) {
+      return cb(err);
+    }
+    cb();
+  })
 };
 
 
