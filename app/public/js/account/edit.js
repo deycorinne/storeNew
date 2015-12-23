609b8900858a3ec1ae852286987e33d392b05f4e
Corinne <Corinne@Podiums-MBP.hsd1.md.comcast.net>
2015-12-23 12:02:50 -0500

    static navbar

--- app/public/js/account/edit.js
+++ app/public/js/account/edit.js
@@ -9,90 +9,106 @@
 /******************FUNCTION CALLED ON SUBMIT********************/
 
 function formValidation() {
-    var firstname = document.getElementById('firstname').value;
-    var lastname = document.getElementById('lastname').value;
-    var phone = document.getElementById('phone').value;
-    var address = document.getElementById('address').value;
-    var postcode = document.getElementById('postcode').value;
+  var email = document.getElementById('email').value;
+  var firstname = document.getElementById('firstname').value;
+  var lastname = document.getElementById('lastname').value;
+  var phone = document.getElementById('phone').value;
+  var address = document.getElementById('address').value;
+  var postcode = document.getElementById('postcode').value;
 
-    // nest all supporting functions, if all true then send thank you alert
+  // nest all supporting functions, if all true then send thank you alert
+  if (email_validation(email)) {
     if (firstname_validation(firstname)) {
-        if (lastname_validation(lastname)) {
-            if (phone_validation(phone)) {
-                if (address_validation(address)) {
-                    if (postcode_validation(postcode)) {
-                        alert('Thank you for submitting the form.');
-                        document.getElementById("edit_account").submit();
-                    }
-                }
+      if (lastname_validation(lastname)) {
+        if (phone_validation(phone)) {
+          if (address_validation(address)) {
+            if (postcode_validation(postcode)) {
+              alert('Thank you for submitting the form.');
+              document.getElementById("edit_account").submit();
             }
+          }
         }
+      }
     }
+  }
 
 }
 
 
 /******************SUPPORT FUNCTIONS********************/
 
+function email_validation(em) {
+
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (em.match(mailformat)) {
+    return true;
+  } else {
+    alert('You have entered an invalid email address.');
+    return false;
+  }
+
+}
+
 function firstname_validation(fn) {
 
-    var fn_len = fn.length;
+  var fn_len = fn.length;
 
-    if (fn_len === 0) {
-        alert("First name field should not be empty!");
-        return false;
-    } else {
-        return true;
-    }
+  if (fn_len === 0) {
+    alert("First name field should not be empty!");
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function lastname_validation(ln) {
 
-    var ln_len = ln.length;
+  var ln_len = ln.length;
 
-    if (ln_len === 0) {
-        alert("Last name field should not be empty!");
-        return false;
-    } else {
-        return true;
-    }
+  if (ln_len === 0) {
+    alert("Last name field should not be empty!");
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function phone_validation(ph) {
-    // Note: making type number means it cannot be submitted unless
-    // the value is only numbers, so no check necessary with phone
-    // or postcode
+  // Note: making type number means it cannot be submitted unless
+  // the value is only numbers, so no check necessary with phone
+  // or postcode
 
-    var phone_len = ph.length;
+  var phone_len = ph.length;
 
-    if (phone_len < 10) {
-        alert('Phone number must be ten numbers.');
-        return false;
-    } else {
-        return true;
-    }
+  if (phone_len < 10) {
+    alert('Phone number must be ten numbers.');
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function address_validation(ad) {
 
-    var ad_len = ad.length;
+  var ad_len = ad.length;
 
-    if (ad_len === 0) {
-        alert("Address should not be empty!");
-        return false;
-    } else {
-        return true;
-    }
+  if (ad_len === 0) {
+    alert("Address should not be empty!");
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function postcode_validation(pc) {
 
-    var post_len = pc.length;
+  var post_len = pc.length;
 
-    if (post_len < 5) {
-        alert('Post code must be five numbers.');
-        return false;
-    } else {
-        return true;
-    }
+  if (post_len < 5) {
+    alert('Post code must be five numbers.');
+    return false;
+  } else {
+    return true;
+  }
 }
--- app/server/controllers/account/controller.js
+++ app/server/controllers/account/controller.js
@@ -4,37 +4,61 @@
 
 var User = require('../../models/user/model.js');
 
-exports.account = function (req, res) {
-    var breadcrumbs = [
-        {title: 'Dashboard', link: '/dashboard/'},
-        {title: 'Account'}
-    ];
-    res.render('account/list', {title: 'My Account', breadcrumbs: breadcrumbs});
+exports.account = function(req, res) {
+  var breadcrumbs = [{
+    title: 'Dashboard',
+    link: '/dashboard/'
+  }, {
+    title: 'Account'
+  }];
+  res.render('account/list', {
+    title: 'My Account',
+    breadcrumbs: breadcrumbs
+  });
 };
 
-exports.updateForm = function (req, res) {
-    var breadcrumbs = [
-        {title: 'Dashboard', link: '/dashboard/'},
-        {title: 'Account', link: '/account/'},
-        {title: 'Edit Account'}
-    ];
+exports.updateForm = function(req, res) {
+  var breadcrumbs = [{
+    title: 'Dashboard',
+    link: '/dashboard/'
+  }, {
+    title: 'Account',
+    link: '/account/'
+  }, {
+    title: 'Edit Account'
+  }];
 
-    res.render('account/forms/account', {title: 'Edit Account', breadcrumbs: breadcrumbs});
+  res.render('account/forms/account', {
+    title: 'Edit Account',
+    breadcrumbs: breadcrumbs
+  });
 };
 
-exports.update = function (req, res) {
-    User.findOne({email: req.user.email}, function (err, user) {
-        if (err) {console.log(err);}
+exports.update = function(req, res) {
+  User.findOne({
+    email: req.user.email
+  }, function(err, user) {
+    if (err) {
+      console.log(err);
+    }
 
-        user.firstname = req.body.firstname;
-        user.lastname = req.body.lastname;
-        user.phone = req.body.phone;
-        user.address = req.body.address;
-        user.postcode = req.body.postcode;
+    user.firstname = req.body.firstname;
+    user.email = req.body.email;
+    user.lastname = req.body.lastname;
+    user.phone = req.body.phone;
+    user.address = req.body.address;
+    user.postcode = req.body.postcode;
 
-        user.save();
-
-        res.writeHead(302, {'Location': '/account'});
-        res.end();
+    user.save(function(err, user) {
+      if (err) {
+        console.log(err);
+      }
+      res.writeHead(302, {
+        'Location': '/account'
+      });
+      res.end();
     });
+
+
+  });
 };
